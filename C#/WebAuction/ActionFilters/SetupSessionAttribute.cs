2f29cb43691b5e200b25fa6a90f05830de4446ec
Chris F <fingarc@gmail.com>
2013-05-24 19:39:47 -0700

    missed files from previous commit

---
+++ C#/WebAuction/ActionFilters/SetupSessionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+namespace Czf.WebAuction.ActionFilters
+{
+	/// <summary>
+	/// Description of SetupSessionAttribute.
+	/// </summary>
+	public class SetupSessionAttribute : ActionFilterAttribute
+	{
+		public SetupSessionAttribute()
+		{
+			
+		}
+	}
+}
---
+++ C#/WebAuction/Models/LoginActionViewModel.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Chris
+ * Date: 5/7/2013
+ * Time: 9:35 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace WebAuction.Models
+{
+	/// <summary>
+	/// Data pertaining to login.
+	/// </summary>
+	public class LoginActionViewModel
+	{
+		public LoginActionViewModel()
+		{
+		}
+	}
+}

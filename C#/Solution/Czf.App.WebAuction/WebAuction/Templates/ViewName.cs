c1e1a8778a9706767694fcad4b9ae66f43622d5d
Chris F <fingarc@gmail.com>
2014-12-19 19:13:55 -0800

    Now gets sibling folders.

--- C#/Solution/Czf.App.WebAuction/WebAuction/Templates/ViewName.cs
+++ C#/Solution/Czf.App.WebAuction/WebAuction/Templates/ViewName.cs
@@ -5,39 +5,62 @@ namespace Czf.App.WebAuction
 	///<summary> Static class for view names</summary>
 	public static class ViewName 
 	{
- 		public static class Home
- 		{
- 			public static class Partials
- 			{					
+		/// <summary>Directory</summary>
+		public static class Shared
+		{
+			/// <summary>Directory</summary>
+			public static class Header
+			{
+				/// <summary>Directory</summary>
+				public static class DepthTest
+				{
+					/// <summary>Full Views for this directory</summary>
+					public static class FullView
+					{				
+						/// <summary>String constant for the depthTestView View.</summary>
+						public static readonly string depthTestView = "depthTestView";
+					}
+				}
+ 
+	 			///<summary>Partial Views for this directory</summary>
+	 			public static class PartialView
+	 			{				
+					/// <summary>String constant for the _header partial.</summary>
+					public static readonly string _header = "_header";				
+				}
 			}
-			public static class FullView
-			{				
-				/// <summary>String constant for the Contact View.</summary>
-				public static readonly string Contact = "Contact";			
-				/// <summary>String constant for the Index View.</summary>
-				public static readonly string Index = "Index";			
-			}
-		}
- 		public static class Login
- 		{
- 			public static class Partials
- 			{					
-			}
-			public static class FullView
-			{				
-				/// <summary>String constant for the Index View.</summary>
-				public static readonly string Index = "Index";			
-			}
-		}
- 		public static class Shared
- 		{
- 			public static class Partials
+ 
+ 			///<summary>Partial Views for this directory</summary>
+ 			public static class PartialView
  			{				
 				/// <summary>String constant for the _LayoutPage partial.</summary>
 				public static readonly string _LayoutPage = "_LayoutPage";				
 			}
+		}
+ 
+		/// <summary>Directory</summary>
+		public static class Login
+		{
+
+			/// <summary>Full Views for this directory</summary>
 			public static class FullView
 			{				
+				/// <summary>String constant for the Index View.</summary>
+				public static readonly string Index = "Index";
+			}
+		}
+ 
+		/// <summary>Directory</summary>
+		public static class Home
+		{
+
+			/// <summary>Full Views for this directory</summary>
+			public static class FullView
+			{				
+				/// <summary>String constant for the Contact View.</summary>
+				public static readonly string Contact = "Contact";				
+				/// <summary>String constant for the Index View.</summary>
+				public static readonly string Index = "Index";
 			}
 		}
  

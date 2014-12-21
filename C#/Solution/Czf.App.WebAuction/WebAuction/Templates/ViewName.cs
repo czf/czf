9561d36aa7383f9a347e6b5c6bb0ac4e7b0a8314
Chris F <fingarc@gmail.com>
2014-12-20 17:59:56 -0800

    remove ViewName.txt. added viewname.tt to before build texttemplate generation.

--- C#/Solution/Czf.App.WebAuction/WebAuction/Templates/ViewName.cs
+++ C#/Solution/Czf.App.WebAuction/WebAuction/Templates/ViewName.cs
@@ -31,51 +31,7 @@ namespace Czf.App.WebAuction
 
 		/// <summary>Directory</summary>
 		public static class Shared
-		{
-			/// <summary>Directory</summary>
-			public static class Footer
-			{ 
-	 			///<summary>Partial Views for this directory</summary>
-	 			public static class PartialView
-	 			{				
-					/// <summary>String constant for the _footer partial.</summary>
-					public static readonly string _footer = "_footer";				
-				}
-			}
-
-			/// <summary>Directory</summary>
-			public static class Header
-			{
-				/// <summary>Directory</summary>
-				public static class DepthTest
-				{
-					/// <summary>Full Views for this directory</summary>
-					public static class FullView
-					{				
-						/// <summary>String constant for the depthTestView View.</summary>
-						public static readonly string depthTestView = "depthTestView";
-					}
-				}
-
-				/// <summary>Directory</summary>
-				public static class SameDepthTest
-				{
-					/// <summary>Full Views for this directory</summary>
-					public static class FullView
-					{				
-						/// <summary>String constant for the sameDepthTest View.</summary>
-						public static readonly string sameDepthTest = "sameDepthTest";
-					}
-				}
- 
-	 			///<summary>Partial Views for this directory</summary>
-	 			public static class PartialView
-	 			{				
-					/// <summary>String constant for the _header partial.</summary>
-					public static readonly string _header = "_header";				
-				}
-			}
- 
+		{ 
  			///<summary>Partial Views for this directory</summary>
  			public static class PartialView
  			{				

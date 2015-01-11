20d3963310517652923b03fff7764fa9fb55513b
Chris F <fingarc@gmail.com>
2015-01-10 20:28:46 -0800

    Stylesheet assets are written to the cs file

---
+++ C#/Solution/Czf.App.WebAuction/WebAuction/Templates/Assets.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+//get all the files
+	//filter out the merge and minify files
+//add to appropriate section
+
+
+using System;
+
+namespace Czf.App.WebAuction
+{
+	public static class Asset
+	{
+		//string bleh = @"D:\dev\solutions\czf\C#\Solution\Czf.App.WebAuction\WebAuction\Templates";
+	 
+		public static class Stylesheet
+		{
+			
+			
+			static Stylesheet()
+			{
+				css_global = assetConfig.Stylesheets[@"css\global.css"];
+				css_Site = assetConfig.Stylesheets[@"css\Site.css"];
+				css_StyleSheetNotFound = assetConfig.Stylesheets[@"css\StyleSheetNotFound.css"];
+			}
+
+			public static readonly string css_global;
+			public static readonly string css_Site;
+			public static readonly string css_StyleSheetNotFound;
+
+		}
+
+	  
+	  True
+	  	  	  	  
+	}
+
+}
+
--- C#/Solution/Czf.Util.Common/StringExtensions.cs
+++ C#/Solution/Czf.Util.Common/StringExtensions.cs
@@ -17,9 +17,36 @@ namespace Czf.Util.Common
 	{
 		#region methods
 		
+		/// <summary>
+		/// if the string is null empty is returned
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
 		public static string NullToEmpty(this string input)
 		{
-			return input == null ? string.Empty : input;
+			return input ?? string.Empty;
+		}
+		
+		/// <summary>
+		/// Returns the first part of the strings, up until the character c. If c is not found in the
+		/// string the whole string is returned.
+		/// </summary>
+		/// <param name="s">String to truncate</param>
+		/// <param name="c">Character to stop at.</param>
+		/// <returns>Truncated string</returns>
+		/// <remarks>
+		/// <para>http://extensionmethod.net/csharp/string/leftof</para>
+		/// Author: Gaston Verelst
+		/// </remarks>
+		public static string LeftOf(this string s, char c)
+		{
+		    int ndx = s.IndexOf(c);
+		    if (ndx >= 0)
+		    {
+		    return s.Substring(0, ndx);
+		    }
+		 
+		    return s;
 		}
 		
 		#endregion
